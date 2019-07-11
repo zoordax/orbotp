38748b0b28e7f62be6e456ff949f60a07836e9b7
zoordax <49092704+zoordax@users.noreply.github.com>
2019-07-11 21:35:28 +0200

    Update bot.py

--- .gitignore/bot.py
+++ .gitignore/bot.py
@@ -57,7 +57,7 @@ async def on_message(message):
     if str( message.channel ) in channels :
         if message.content == "!monstres":
             embed = discord.Embed( title="**Monstre**" , description="Pour utiliser le bot taper : \n **!le_monstre**\nsi vous voulez un exemple pour utiliser le bot taper : \n!exemple"  , color = 0xff1c21 )
-            embed.add_field( name="***>>>***Tous les monstres***<<<***" , value="\n ailedegivre \nailenoires\nbetedesneiges\nchaman\ndrider\nepinator\ngargantua\ngolem\ngriffon\nfaucheuse\nmorphalange\ntroyen\nlarve\nnoceros\nreineabeille\nsabrecroc\ntitan\nwyrm" )
+            embed.add_field( name="***>>>***Tous les monstres***<<<***" , value="\n ailedegivre \nailenoire \nbetedesneiges\nchaman\ndrider\nepinator\ngargantua\ngolem\ngriffon\nfaucheuse\nmorphalange\ntroyen\nlarve\nnoceros\nreineabeille\nsabrecroc\ntitan\nwyrm" )
             await message.channel.send( content=None , embed=embed , delete_after = 60 )
             await asyncio.sleep(60)
             await message.delete()
